3dfbef795ce398e018ff54f342d46fd182831da6
Dos Moonen <darsstar@gmail.com>
2024-03-29 15:35:22 +0000

    WIP

--- homeassistant/components/solax/sensor.py
+++ homeassistant/components/solax/sensor.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from datetime import datetime, timedelta
+from typing import Any
 
 from solax import RealTimeAPI
 from solax.inverter import InverterError
@@ -24,7 +25,7 @@ from homeassistant.const import (
     UnitOfPower,
     UnitOfTemperature,
 )
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import CALLBACK_TYPE, HomeAssistant, callback
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -37,6 +38,7 @@ from homeassistant.util import dt as util_dt
 from .const import DOMAIN, MANUFACTURER
 
 DEFAULT_PORT = 80
+UNAVAILABLE_MULTIPLIER = 10
 SCAN_INTERVAL = timedelta(seconds=5)
 
 
@@ -111,15 +113,14 @@ async def async_setup_entry(
     resp = await api.get_data()
     serial = resp.serial_number
     version = resp.version
-    last_reset = util_dt.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    endpoint = RealTimeDataEndpoint(hass, api)
+    last_reset = util_dt.start_of_local_day()
+    endpoint = RealTimeDataEndpoint(hass, api, SCAN_INTERVAL)
     entry.async_create_background_task(
         hass, endpoint.async_refresh(), f"solax {entry.title} initial refresh"
     )
     entry.async_on_unload(
         async_track_time_interval(hass, endpoint.async_refresh, SCAN_INTERVAL)
     )
-    devices = []
     for sensor, (idx, measurement) in api.inverter.sensor_map().items():
         description = SENSOR_DESCRIPTIONS[
             (measurement.unit, measurement.is_monotonic, measurement.storage)
@@ -146,19 +147,33 @@ async def async_setup_entry(
                 second=0,
             )
 
-        devices.append(device)
-    endpoint.sensors = devices
-    async_add_entities(devices)
+        endpoint.sensors.append(device)
+    hass.async_add_job(endpoint.async_refresh)
+    endpoint.cancel_callback = async_track_time_interval(
+        hass, endpoint.async_refresh, SCAN_INTERVAL
+    )
+    async_add_entities(endpoint.sensors)
 
 
 class RealTimeDataEndpoint:
     """Representation of a Sensor."""
 
-    def __init__(self, hass: HomeAssistant, api: RealTimeAPI) -> None:
+    hass: HomeAssistant
+    api: RealTimeAPI | None
+    sensors: list[Inverter]
+    scan_interval: timedelta
+    last_success: datetime
+    cancel_callback: CALLBACK_TYPE
+
+    def __init__(
+        self, hass: HomeAssistant, api: RealTimeAPI, scan_interval: timedelta
+    ) -> None:
         """Initialize the sensor."""
         self.hass = hass
         self.api = api
-        self.sensors: list[Inverter] = []
+        self.sensors = []
+        self.scan_interval = scan_interval
+        self.last_success: datetime = util_dt.as_local(datetime.fromtimestamp(0))
 
     async def async_refresh(self, now=None):
         """Fetch new state data for the sensor.
@@ -167,6 +182,9 @@ class RealTimeDataEndpoint:
         """
         try:
             api_response = await self.api.get_data()
+
+            last_success, self.last_success = (self.last_success, now or util_dt.now())
+
             if (
                 not api_response
                 or "Total Energy" not in api_response.data
@@ -181,15 +199,29 @@ class RealTimeDataEndpoint:
                     logging.info(f"{api_response.version=}")
                 else:
                     logging.info(f"{api_response=}")
-        except (InverterError, TimeoutError) as err:
-            if now is not None:
+
+            if (
+                now is None
+                or now - self.scan_interval * UNAVAILABLE_MULTIPLIER > last_success
+            ):
+                # might indicate the inverter just came online / out of standby
+                # give it one cycle to initialize
+                import logging
+
+                logging.info("give inverter one cycle to initialize")
                 return
-            raise PlatformNotReady from err
-        data = api_response.data
+
+        except (InverterError, TimeoutError) as err:
+            if now is None:
+                raise PlatformNotReady from err
+            available = False
+            data = {}
+        else:
+            available = True
+            data = api_response.data
+
         for sensor in self.sensors:
-            if sensor.key in data:
-                sensor.value = data[sensor.key]
-                sensor.async_schedule_update_ha_state()
+            sensor.refresh(available, data)
 
 
 class Inverter(SensorEntity):
@@ -226,15 +258,23 @@ class Inverter(SensorEntity):
         self.value = None
 
     @callback
-    def async_listen_for_midnight(self, midnight: datetime) -> None:
+    def async_listen_for_midnight(self, today: datetime) -> None:
         """Reset at midnight."""
-        import logging
+        # del self.last_reset
+        self._attr_last_reset = util_dt.start_of_local_day(today)
+        self.value = 0
+        self.async_schedule_update_ha_state()
 
-        del self.last_reset
-        logging.info("midnight: %s", midnight)
-        self._attr_last_reset = midnight
-        # self.value = 0
-        # self.async_schedule_update_ha_state()
+    def refresh(self, available: bool, data: dict[str, Any]) -> None:
+        """Refresh this entity."""
+        if not available:
+            self._attr_available = False
+        elif self.key in data:
+            self._attr_available = True
+            self.value = data[self.key]
+        else:
+            return
+        self.async_schedule_update_ha_state()
 
     @property
     def native_value(self):
